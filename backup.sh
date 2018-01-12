d22a0de407ade74a5f77397a112ec517f5360544
Christopher Sean Lovell <cslovell@users.noreply.github.com>
2018-01-12 14:08:12 +0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker exec -u postgres sdgdatahubruntime_postgres_1 pg_dump --format=custom -d ckan > ckan.dump
-aws s3 cp ckan.dump s3://sdgdatahub-backups/ckan.dump
+docker exec -u postgres sdgdatahubruntime_postgres_1 pg_dump --format=custom -d ckan > /home/ec2-user/ckan.dump
+aws s3 cp /home/ec2-user/ckan.dump s3://sdgdatahub-backups/ckan.dump
