709feb4943d1979abed8dfbc52986c1ae60288a8
Christopher Sean Lovell <cslovell@users.noreply.github.com>
2018-01-12 14:09:14 +0700

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-aws s3 cp s3://sdgdatahub-backups/ckan.dump ckan.dump
+aws s3 cp s3://sdgdatahub-backups/ckan.dump /home/ec2-user/ckan.dump
 docker exec ckan paster --plugin=ckan db clean -c /etc/ckan/default/default.ini
-docker cp ckan.dump sdgdatahubruntime_postgres_1:/home/
+docker cp /home/ec2-user/ckan.dump sdgdatahubruntime_postgres_1:/home/
 docker exec -u postgres sdgdatahubruntime_postgres_1 pg_restore -v --clean --if-exists -d ckan /home/ckan.dump
